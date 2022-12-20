5aba488abc6781f959b9ee9ff1e93eeba39c9c2d
Widz <widz@gmail.com>
2022-12-20 12:03:01 -0800

    Use multiple inheritence

--- inheritence.py
+++ inheritence.py
@@ -2,9 +2,15 @@ class Car:
     def start(self):
         print("Started!!")
 
-class TopDrop(Car):
+class Truck:
+    def load(self):
+        print("Loaded!!")
+
+
+class TopDrop(Car, Truck):
     pass
 
 my_top_drop = TopDrop()
 
 my_top_drop.start()
+my_top_drop.load()
